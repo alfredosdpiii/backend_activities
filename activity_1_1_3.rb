f196e7c1b9c0696c030bf48daa0bdadb70905213
alfredosdpiii <alfredo.sdpiii@gmail.com>
2022-03-30 20:24:42 +0800

    renamed

--- activity_1_1_3.rb
+++ activity_1_1_3.rb
@@ -1,10 +1,10 @@
-contact_data = [
+user_data = [
   ['john@email.com', '123 Main st.', '555-123-4567'],
   ['avion@gmail.com', '404 Not Found Dr.', '123-234-3454']
 ]
 
-contacts = { 'John Cruz' => {}, 'Avion School' => {} }
+users = { 'John Cruz' => {}, 'Avion School' => {} }
 
-contacts['John Cruz'] = contact_data[0]
-contacts['Avion School'] = contact_data[1]
-puts contact_data
+users['John Cruz'] = user_data[0]
+users['Avion School'] = user_data[1]
+puts user_data
