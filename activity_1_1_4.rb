55f4399e7ad666b6535396294ae92cd504a8e245
alfredosdpiii <alfredo.sdpiii@gmail.com>
2022-03-30 20:13:39 +0800

    Solution for number 4

---
+++ activity_1_1_4.rb
@@ -0,0 +1,7 @@
+puts 'number 4'
+puts 'what is your age?'
+age = gets.to_i
+puts "In 10 years you will be #{age + 10}"
+puts "In 10 years you will be #{age + 20}"
+puts "In 10 years you will be #{age + 30}"
+puts "In 10 years you will be #{age + 40}"
