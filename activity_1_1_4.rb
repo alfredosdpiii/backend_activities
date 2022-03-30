eda4c81ddf743ce488b4098dc387e4ea7f38a6d2
alfredosdpiii <alfredo.sdpiii@gmail.com>
2022-03-30 20:19:22 +0800

    fixed typo

--- activity_1_1_4.rb
+++ activity_1_1_4.rb
@@ -2,6 +2,6 @@ puts 'number 4'
 puts 'what is your age?'
 age = gets.to_i
 puts "In 10 years you will be #{age + 10}"
-puts "In 10 years you will be #{age + 20}"
-puts "In 10 years you will be #{age + 30}"
-puts "In 10 years you will be #{age + 40}"
+puts "In 20 years you will be #{age + 20}"
+puts "In 30 years you will be #{age + 30}"
+puts "In 40 years you will be #{age + 40}"
